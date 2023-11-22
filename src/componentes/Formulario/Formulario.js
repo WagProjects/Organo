ec9f797498dfb154d55acaf774c5112f1ef294ba
Wagner Oliveira <contato-wagneros@outlook.com>
2023-11-22 15:08:24 -0300

    formulario

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -2,6 +2,7 @@ import './Formulario.css'
 import CampoTexto from '../CampoTexto/CampoTexto.js'
 import ListaSupensa from '../ListaSuspensa/ListaSuspensa.js'
 import Botao from '../Botao/Botao.js'
+import { useState } from 'react'
 
 const Formulario = () => {
 
@@ -15,18 +16,24 @@ const Formulario = () => {
         'Inovação e Gestão'
     ]
 
+    const [nome, setNome] = useState('')
+    const [cargo, setCargo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [time, setTime] = useState('')
+
     const aoSalva = (evento) =>{
         evento.preventDefault()
+        console.log('Form foi submetido =>', nome, cargo, imagem, time)
     }
 
     return (
         <section className='formulario'>
             <form onSubmit={aoSalva}>
                 <h2>Preencha os dados para criar o card do colaborador.</h2>
-                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome" />
-                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" />
-                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Informe o endereço da imagem" />
-                <ListaSupensa obrigatorio={true} label="Time" itens={times}/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu Nome" valor={nome} aoAlterado={valor => setNome(valor)} />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo" valor={cargo} aoAlterado={valor => setCargo(valor)} />
+                <CampoTexto obrigatorio={true} label="Imagem" placeholder="Informe o endereço da imagem" valor={imagem} aoAlterado={valor => setImagem(valor)} />
+                <ListaSupensa obrigatorio={true} label="Time" itens={times} valor={time} aoAlterado={valor => setTime(valor)}/>
                 <Botao>Criar Card</Botao>
             </form>
         </section>
