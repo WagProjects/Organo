f5ae3945d84f116e7fcfa964ed85db0720f42a4a
Wagner Oliveira <contato-wagneros@outlook.com>
2023-11-28 17:53:29 -0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Banner from './componentes/Banner/Banner.js';
 import Formulario from './componentes/Formulario/Formulario.js';
 import Time from './componentes/Time/Time.js';
+import Rodape from './componentes/Rodape/Rodape.js';
 
 
 function App() {
@@ -63,6 +64,7 @@ function App() {
       corSecundaria={time.corSecundaria}
       colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
       />)}
+      <Rodape/>
     </div>
   );
 }
---
+++ src/componentes/Rodape/Rodape.js
@@ -0,0 +1,17 @@
+import './Rodape.css'
+
+function Rodape() {
+    return(
+        <footer className='rodape'>
+            <section>
+                <ul>
+                    <li>
+                        <img src='../../../imagens/fb.png' alt='facebook'/>
+                    </li>
+                </ul>
+            </section>
+        </footer>
+    )
+}
+
+export default Rodape
